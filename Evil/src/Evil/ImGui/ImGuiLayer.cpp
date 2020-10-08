938680a602672acdcecc03b73d7d8995aafcb807
soloexitus <soloexitus@gmail.com>
2020-10-08 12:17:19 +0300

    Our first Triangle!

--- Evil/src/Evil/ImGui/ImGuiLayer.cpp
+++ Evil/src/Evil/ImGui/ImGuiLayer.cpp
@@ -35,8 +35,8 @@ namespace Evil
 		//io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoMerge;
 
 		// Setup Dear ImGui style
-		//ImGui::StyleColorsDark();
-		ImGui::StyleColorsClassic();
+		ImGui::StyleColorsDark();
+		//ImGui::StyleColorsClassic();
 
 		// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
 		ImGuiStyle& style = ImGui::GetStyle();
--- Evil/src/Evil/Platform/OpenGL/OpenGLContext.cpp
+++ Evil/src/Evil/Platform/OpenGL/OpenGLContext.cpp
@@ -17,6 +17,11 @@ namespace Evil
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		EVIL_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		EVIL_CORE_INFO("OpenGL info:");
+		EVIL_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		EVIL_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		EVIL_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
