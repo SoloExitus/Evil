70319bdedaa86470305fa06a7f4e578d2c49fc49
soloexitus <soloexitus@gmail.com>
2020-11-22 21:53:26 +0300

    Added the ability to load shaders from files and a few fixes.

--- premake5.lua
+++ premake5.lua
@@ -8,6 +8,11 @@ workspace "Evil"
 		"Dist"
 	}
 
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Include directories relative to root folder (solution directory)
