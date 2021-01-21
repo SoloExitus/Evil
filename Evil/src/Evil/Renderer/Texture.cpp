e394d378469f954cecf0ecb51d58e2dc2c830aad
Exitus <Exitus@MAIN>
2021-01-21 11:46:19 +0300

    Renderer2D now uses a single shader to render both textured and non-textured quads.

--- Evil/src/Evil/Renderer/Texture.cpp
+++ Evil/src/Evil/Renderer/Texture.cpp
@@ -6,17 +6,28 @@
 
 namespace Evil
 {
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:		EVIL_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+			case RendererAPI::API::OpenGL:		return CreateRef<OpenGLTexture2D>(width,height);
+		}
+
+		EVIL_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<Texture2D> Texture2D::Create(const std::string& path)
 	{
 		
 		switch (Renderer::GetAPI())
 		{
 			case RendererAPI::API::None:		EVIL_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-			case RendererAPI::API::OpenGL:		return std::make_shared<OpenGLTexture2D>(path);
+			case RendererAPI::API::OpenGL:		return CreateRef<OpenGLTexture2D>(path);
 		}
 		
 		EVIL_CORE_ASSERT(false, "Unknown RendererAPI!");
 		return nullptr;
-		
 	}
 }
