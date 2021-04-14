9b47c3ee499289e7d6748ebf5be3e22161a7fa89
Exitus <Exitus@MAIN>
2021-04-14 09:59:12 +0300

    Added Properties panel.

--- EvilEditor/src/EditorLayer.cpp
+++ EvilEditor/src/EditorLayer.cpp
@@ -41,27 +41,31 @@ namespace Evil
 		class CameraController : public ScriptableEntity
 		{
 		public:
-			void OnCreate()
+			virtual void OnCreate() override
 			{
 			}
 
-			void OnDestroy()
+			virtual void OnDestroy() override
 			{
 			}
 
-			void OnUpdate(Timestep ts)
+			virtual void OnUpdate(Timestep ts) override
 			{
-				auto& transform = GetComponent<TransformComponent>().Transform;
-				float speed = 5.0f;
+				if (GetComponent<CameraComponent>().Primary)
+				{
+					auto& transform = GetComponent<TransformComponent>().Transform;
+					float speed = 5.0f;
 
-				if (Input::IsKeyPressed(Key::A))
-					transform[3][0] -= speed * ts;
-				if (Input::IsKeyPressed(Key::D))
-					transform[3][0] += speed * ts;
-				if (Input::IsKeyPressed(Key::W))
-					transform[3][1] += speed * ts;
-				if (Input::IsKeyPressed(Key::S))
-					transform[3][1] -= speed * ts;
+					if (Input::IsKeyPressed(Key::A))
+						transform[3][0] -= speed * ts;
+					if (Input::IsKeyPressed(Key::D))
+						transform[3][0] += speed * ts;
+					if (Input::IsKeyPressed(Key::W))
+						transform[3][1] += speed * ts;
+					if (Input::IsKeyPressed(Key::S))
+						transform[3][1] -= speed * ts;
+				}
+				
 			}
 		};
 
