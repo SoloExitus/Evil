8508615e8ee6d50e8bd0f366c84c8fc788bfef22
Exitus <Exitus@MAIN>
2021-04-27 08:55:57 +0300

    Some improvements: - Prevent crash when user adds a component that is already present - If the extension is not specified, the file was not saved with a .evil extension - Change to FileDialogs' methods so that they return std::optional - Use YAML::LoadFile to load scene files - Change strcpy_s to std::strncpy to comply with cpp standards

--- Evil/src/Evil/Platform/Windows/WindowsPlatformUtils.cpp
+++ Evil/src/Evil/Platform/Windows/WindowsPlatformUtils.cpp
@@ -1,6 +1,7 @@
 #include "evilpch.h"
 #include "Evil/Utils/PlatformUtils.h"
 
+#include <sstream>
 #include <commdlg.h>
 #include <GLFW/glfw3.h>
 #define GLFW_EXPOSE_NATIVE_WIN32
@@ -10,7 +11,7 @@
 
 namespace Evil {
 
-	std::string FileDialogs::OpenFile(const char* filter)
+	std::optional<std::string> FileDialogs::OpenFile(const char* filter)
 	{
 		OPENFILENAMEA ofn;
 		CHAR szFile[260] = { 0 };
@@ -23,13 +24,12 @@ namespace Evil {
 		ofn.nFilterIndex = 1;
 		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
 		if (GetOpenFileNameA(&ofn) == TRUE)
-		{
 			return ofn.lpstrFile;
-		}
-		return std::string();
+		
+		return std::nullopt;
 	}
 
-	std::string FileDialogs::SaveFile(const char* filter)
+	std::optional<std::string> FileDialogs::SaveFile(const char* filter)
 	{
 		OPENFILENAMEA ofn;
 		CHAR szFile[260] = { 0 };
@@ -40,12 +40,15 @@ namespace Evil {
 		ofn.nMaxFile = sizeof(szFile);
 		ofn.lpstrFilter = filter;
 		ofn.nFilterIndex = 1;
+
+		// Sets the default extension by extracting it from the filter
+		ofn.lpstrDefExt = std::strchr(filter, '\0') + 1;
+
 		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
 		if (GetSaveFileNameA(&ofn) == TRUE)
-		{
 			return ofn.lpstrFile;
-		}
-		return std::string();
+		
+		return std::nullopt;
 	}
 
 }
--- Evil/src/Evil/Utils/PlatformUtils.h
+++ Evil/src/Evil/Utils/PlatformUtils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <optional>
 
 namespace Evil {
 
@@ -8,8 +9,8 @@ namespace Evil {
 	{
 	public:
 		// These return empty strings if cancelled
-		static std::string OpenFile(const char* filter);
-		static std::string SaveFile(const char* filter);
+		static std::optional<std::string> OpenFile(const char* filter);
+		static std::optional<std::string> SaveFile(const char* filter);
 	};
 
 }
